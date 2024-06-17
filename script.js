bfba93a2b49f0cd1d179dce3090f6f40e63cb625
Mario Ramirez <mario-ramirez@hotmail.de>
2024-06-17 15:47:41 +0200

    test

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ console.log("Hallo");
 // Error! Make it work
 console.log("That doesn t work");
 // should return true. Change only the operator to fix it.
-console.log(50 + 2 > !52);
+console.log(50 + 2 == 52);
 // should be false. Change only the operator to fix it.
-console.log(333 == 333);
+console.log("333" != 333);
 // Wrong result: Expected 555 not 855. Change only the operator to fix it.
 console.log((600 + 510) / 2);
 // Wrong result: Expected a Number 4 received 22
